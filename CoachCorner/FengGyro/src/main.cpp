d631712774d8f668629109bb1a544956438379a2
CoachN <34515736+ScientificCodingKids@users.noreply.github.com>
2021-01-17 23:52:11 -0500

    Remove code already moved to nutil
    todo: update makefile

--- CoachCorner/FengGyro/src/main.cpp
+++ CoachCorner/FengGyro/src/main.cpp
@@ -11,10 +11,7 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
-#include <vector>
-#include <algorithm>
-#include <cstdarg>
-#include <cstdio>
+#include "utils/nutils.hpp"
 
 using namespace vex;
 competition Competition;
@@ -23,41 +20,6 @@ competition Competition;
 
 double x = 0.5;
 
-class ScrollingScreen {
-  public:
-    void print(const char* s...);
-  private:
-    std::vector<std::string> _rows;
-};
-
-void ScrollingScreen::print(const char* fmt...) {
-  char buf[1024];
-
-  va_list args;
-  va_start(args, fmt);
-
-  vsnprintf(buf, 1024, fmt, args);
-  _rows.push_back(std::string(buf));
-
-  int K = 12;
-
-  int n = _rows.size() - K;
-
-  if (n < 0) n = 0;
-
-  int m =  _rows.size();
-
-  if (m > K) m = K;
-
-  Brain.Screen.clearScreen();
-
-  for (unsigned int i=0; i<m; ++i) {
-    Brain.Screen.print( _rows[n+i].c_str() );
-    Brain.Screen.newLine();
-  }
-
-  va_end(args);
-}
 
 auto ss = ScrollingScreen();
 

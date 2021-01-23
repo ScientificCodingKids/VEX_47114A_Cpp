a92da6e7d1ffad09a307106fc57f47d5f9a0e6e9
CoachN <34515736+ScientificCodingKids@users.noreply.github.com>
2021-01-23 14:54:09 -0500

    Remove unneeded comment

--- CoachCorner/FengGyro/vex/mkenv.mk
+++ CoachCorner/FengGyro/vex/mkenv.mk
@@ -97,6 +97,5 @@ LIBS =  --start-group -lv5rt -lstdc++ -lc -lm -lgcc --end-group
 
 # include file paths
 INC += $(addprefix -I, ${INC_F}) 
-#INC += $(addprefix -I, ${INC_F}"/../../..")
 INC += -I"$(TOOLCHAIN)/$(PLATFORM)/include"
 INC += ${TOOL_INC}

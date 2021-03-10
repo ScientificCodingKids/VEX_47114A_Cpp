e714b41b6863ade5d52d94e4d91ea186f94172bc
CoachN <34515736+ScientificCodingKids@users.noreply.github.com>
2021-03-09 21:55:08 -0500

    Make ScrollingScreen configurable

--- Utils/nutils.hpp
+++ Utils/nutils.hpp
@@ -5,15 +5,22 @@
 template<class T>
 class ScrollingScreen {
   public:
-    ScrollingScreen(): _rows() {;}
+    ScrollingScreen(int firstRow=1, int maxRows=12): 
+      _firstRow(firstRow), _maxRows(maxRows), _rows() {;}
+    // LCD screen row is indexed from 1, not 0
+
     void print(const char* s...);
   private:
+    int _firstRow;
+    int _maxRows;
     std::vector<std::string> _rows;
 };
 
 
 template<class T>
 void ScrollingScreen<T>::print(const char* fmt...) {
+  // todo: current implementation uses unbounded memory
+  // need switch to LILO queue only keeping last K elems.
   char buf[1024];
 
   va_list args;
@@ -22,7 +29,7 @@ void ScrollingScreen<T>::print(const char* fmt...) {
   vsnprintf(buf, 1024, fmt, args);
   _rows.push_back(std::string(buf));
 
-  int K = 12;
+  int K = this->_maxRows;
 
   int n = _rows.size() - K;
 
@@ -35,7 +42,7 @@ void ScrollingScreen<T>::print(const char* fmt...) {
   Brain.Screen.clearScreen();
 
   for (unsigned int i=0; i<m; ++i) {
-    Brain.Screen.setCursor(i, 1);
+    Brain.Screen.setCursor(this->_firstRow + i, 1);
     Brain.Screen.print( _rows[n+i].c_str() );
     Brain.Screen.newLine();
   }

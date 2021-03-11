32d3a242d81153aaff7d1c304033eb4b71778d1b
CoachN <34515736+ScientificCodingKids@users.noreply.github.com>
2021-03-10 21:44:06 -0500

    Config: apply same fixes to back motors and inertial sensor addition

--- Play/FirstIntakeDrive/src/robot-config.cpp
+++ Play/FirstIntakeDrive/src/robot-config.cpp
@@ -10,8 +10,6 @@ vex::motor backrightdrive = vex::motor(PORT1);
 vex::motor frontleftdrive = vex::motor(PORT16, true);
 vex::motor frontrightdrive = vex::motor(PORT15);
 
-// vex::drivetrain dt = vex::drivetrain(leftdrive, rightdrive);
-
 vex::motor rightlift = vex::motor(PORT4, true);
 vex::motor leftlift = vex::motor(PORT9);
 
@@ -20,7 +18,7 @@ vex::motor_group lift = vex::motor_group(rightlift, leftlift);
 vex::motor rightintake = vex::motor(PORT20, true);
 vex::motor leftintake = vex::motor(PORT18);
 
-//vex::motor_group intake = vex::motor_group(rightintake, leftintake);
+vex::inertial inertialSensor = vex::inertial(vex::PORT11);
 
 vex::controller rc = vex::controller();
 

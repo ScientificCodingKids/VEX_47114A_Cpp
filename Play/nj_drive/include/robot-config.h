045f84b2fd354a8c5bae5ab6d84605e1d947b471
coco131313 <50887779+coco131313@users.noreply.github.com>
2021-08-22 14:33:45 -0400

    new robot design user control
    created new program (nj_drive) and programmed drivetrain and lift's user control.

---
+++ Play/nj_drive/include/robot-config.h
@@ -0,0 +1,24 @@
+using namespace vex;
+
+extern brain Brain;
+
+extern vex::motor backleftdrive;
+extern vex::motor backrightdrive;
+extern vex::motor frontleftdrive;
+extern vex::motor frontrightdrive;
+
+extern vex::motor leftlift;
+extern vex::motor rightlift;
+
+extern vex::motor_group lift;
+
+extern vex::motor_group dt;
+
+extern vex::controller rc;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);

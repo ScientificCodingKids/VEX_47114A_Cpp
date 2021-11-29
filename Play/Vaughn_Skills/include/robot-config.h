66533c40db08d5efdb818115ab333882b057ced0
coco131313 <50887779+coco131313@users.noreply.github.com>
2021-11-28 21:57:47 -0500

    skills autonomous program (90 pts)

---
+++ Play/Vaughn_Skills/include/robot-config.h
@@ -0,0 +1,31 @@
+using namespace vex;
+
+extern brain Brain;
+
+extern vex::motor backleftdrive;
+extern vex::motor backrightdrive;
+extern vex::motor frontleftdrive;
+extern vex::motor frontrightdrive;
+
+extern vex::motor_group leftdrive;
+extern vex::motor_group rightdrive;
+
+extern vex::drivetrain dt;
+
+extern vex::motor rightlift;
+extern vex::motor leftlift;
+
+extern vex::motor_group lift;
+
+extern vex::motor leftintake;
+extern vex::motor rightintake;
+
+//extern vex::motor_group intake;
+extern vex::controller rc;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
---
+++ Play/Vaughn_Skills/src/main.cpp
@@ -0,0 +1,135 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Chloe Ning                                       */
+/*    Created:      Wed Jan 22 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------xsssssssssss------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    10, 16, 1, 15   
+// lift                 motor_group   4, 9            
+// mogo1                motor         18              
+// mogo2                motor         20              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "../../../Utils/nutils.hpp"
+
+using namespace vex;
+
+competition Competition;
+
+// auto ss = ScrollingScreen<int>();
+
+void pre_auton( void ) {
+}
+
+
+void autonomous( void ) {
+  dt.driveFor(vex::directionType::rev, 78, vex::distanceUnits::in, 60, vex::velocityUnits::pct);
+  dt.driveFor(vex::directionType::fwd, 70, vex::distanceUnits::in, 70, vex::velocityUnits::pct);
+  dt.driveFor(vex::directionType::fwd, 10, vex::distanceUnits::in, 40, vex::velocityUnits::pct);
+  dt.turnFor(vex::turnType::right, 42, vex::rotationUnits::deg, 30, vex::velocityUnits::pct);
+  lift.spinFor(vex::directionType::fwd, 120, vex::rotationUnits::deg);
+  dt.driveFor(vex::directionType::rev, 45, vex::distanceUnits::in, 50, vex::velocityUnits::pct);
+  //12 over
+  dt.driveFor(vex::directionType::rev, 10, vex::distanceUnits::in, 25, vex::velocityUnits::pct);
+  lift.spinFor(vex::directionType::rev, 60, vex::rotationUnits::deg);
+  }
+
+  // before, 45 then 17
+
+void usercontrol( void ) {
+  // int spin = 0;
+
+  while (1) {
+
+    if (rc.ButtonR1.pressing()) {
+      lift.spin(vex::directionType::fwd);
+    }
+    else if (rc.ButtonR2.pressing()) {
+      lift.spin(vex::directionType::rev);
+    }
+
+    else {
+      lift.stop(vex::brakeType::hold);
+    }
+
+    double leftMotorSpeed = rc.Axis3.position(vex::percentUnits::pct) * 0.7;
+    double rightMotorSpeed = rc.Axis2.position(vex::percentUnits::pct) * 0.7;
+
+    if (fabs(leftMotorSpeed) > 5.0) {
+      backleftdrive.setVelocity(leftMotorSpeed, vex::velocityUnits::pct);
+      backleftdrive.spin(fwd);
+      frontleftdrive.setVelocity(leftMotorSpeed, vex::velocityUnits::pct);
+      frontleftdrive.spin(fwd);
+    }
+    else {
+      backleftdrive.stop(vex::brakeType::hold);
+      frontleftdrive.stop(vex::brakeType::hold);
+    }
+
+    if (fabs(rightMotorSpeed) > 5.0) {
+      backrightdrive.setVelocity(rightMotorSpeed, vex::velocityUnits::pct);
+      backrightdrive.spin(fwd);  
+      frontrightdrive.setVelocity(rightMotorSpeed, vex::velocityUnits::pct);
+      frontrightdrive.spin(fwd);
+    }
+    else {
+      backrightdrive.stop(vex::brakeType::hold);
+      frontrightdrive.stop(vex::brakeType::hold);
+    }
+
+    if (rc.ButtonL2.pressing()) {
+      leftintake.setVelocity(10, vex::percentUnits::pct);
+
+      //intake.setVelocity(30, vex::percentUnits::pct);
+      leftintake.spin(vex::directionType::fwd);
+    }
+
+    else if (rc.ButtonL1.pressing()) {
+      
+      leftintake.setVelocity(10, vex::percentUnits::pct);
+      leftintake.spin(vex::directionType::rev);
+
+    }
+
+    else {
+      leftintake.stop(vex::brakeType::hold);
+
+    }
+
+    if (rc.ButtonA.pressing()) {
+      rightintake.setVelocity(10, vex::percentUnits::pct);
+      rightintake.spin(vex::directionType::fwd);
+    }
+    else if (rc.ButtonB.pressing()) {
+      rightintake.setVelocity(10, vex::percentUnits::pct);
+      rightintake.spin(vex::directionType::rev);
+    }
+    else {
+      rightintake.stop(vex::brakeType::hold);
+    }
+
+    vex::task::sleep(50);
+  }
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  Brain.Screen.print("Get ready to start!");
+  Competition.autonomous( autonomous );
+  Competition.drivercontrol( usercontrol );
+
+  pre_auton();
+
+  while(1){
+    vex::task::sleep(100);
+  }
+  return 0;
+}

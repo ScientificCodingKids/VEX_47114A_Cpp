74fc9eff613a4fb2e2e8246ef9bde454ed4f70c6
CoachN <34515736+ScientificCodingKids@users.noreply.github.com>
2022-07-10 11:01:29 -0400

    Remove old code to have fresh start

--- V5/SpinUp/src/main.cpp
+++ V5/SpinUp/src/main.cpp
@@ -1,28 +1,9 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\Chloe Ning                                       */
-/*    Created:      Wed Jan 22 2020                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------xsssssssssss------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    10, 16, 1, 15   
-// lift                 motor_group   4, 9            
-// mogo1                motor         18              
-// mogo2                motor         20              
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
-//#include "C:/Program Files (x86)/VEX Robotics/VEXcode Pro V5/sdk/vexv5/include/vex_units.h"
 #include "vex.h"
 #include <iostream>
 #include <cmath>
-#include "../../../Utils/motion.hpp"
 #include <cassert>
 #include "../../../Utils/nutils.hpp"
+#include "../../../Utils/motion.hpp"
 
 using namespace vex;
 using namespace std;
@@ -31,6 +12,8 @@ competition Competition;
 
 
 void pre_auton( void ) {
+  inertialSensor.calibrate();
+  vex::task::sleep(2000);
 }
 
 void autonomous( void ) {
@@ -44,18 +27,6 @@ void usercontrol( void ) {
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  inertialSensor.calibrate();
-  vex::task::sleep(2500);
-
-  // Coord actualDestLoc = makeTurn(90, true);
-  // SmartScreen ss(Brain.Screen, 3, 6);
-  // ss.printAt(1, "actual loc: (%.2f, %.2f)",actualDestLoc.x, actualDestLoc.y);
-  // vex::task::sleep(3000);
-  // ss.printAt(2, "actual loc after three seconds: (%.2f, %.2f)", actualDestLoc.x, actualDestLoc.y);
-
-  goPlatform();
-  Brain.Screen.print("done");
 
   Competition.autonomous( autonomous );
   Competition.drivercontrol( usercontrol );
